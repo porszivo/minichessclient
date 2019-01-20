e1bfbb1d47b70bb66779f27b406895408bbcbc74
danie <daniel.tertilt@qupe.de>
2019-01-20 14:29:25 +0100

    Added beating and blocking tests for bishops.

--- src/test/java/de/pki/minichess/game/MoveServiceBeatTest.java
+++ src/test/java/de/pki/minichess/game/MoveServiceBeatTest.java
@@ -21,9 +21,9 @@ public class MoveServiceBeatTest extends AbstractMoveServiceTest {
     this.board = new char[][] { //
         { '.', 'I', '.', '.', '.' }, //
         { '.', '.', 'G', 'H', 'J' }, //
-        { 'F', '.', 'X', 'E', '.' }, //
+        { '.', '.', 'X', 'E', '.' }, //
         { 'D', 'A', 'B', '.', '.' }, //
-        { '.', 'C', '.', '.', '.' }, //
+        { '.', 'C', '.', '.', 'F' }, //
         { '.', '.', '.', '.', '.' } };
     this.xPositionOfFigure = 2;
     this.yPositionOfFigure = 2;
@@ -32,7 +32,7 @@ public class MoveServiceBeatTest extends AbstractMoveServiceTest {
   @Test
   public void shouldReturnPossibleWhitePawnMovesOnlyWhileBeating() {
     setFigure('X', 'P');
-    setOtherFigures("qkpppr.r".toCharArray());
+    setOtherFigures("qkpppr.r.....".toCharArray());
     Set<Move> expectedMoves = new HashSet<Move>();
     Square start = new Square(2, 2);
     Square end = new Square(2, 1);
@@ -46,11 +46,11 @@ public class MoveServiceBeatTest extends AbstractMoveServiceTest {
 
     assertThat(compareTwoSets(possibleMovesSet, expectedMoves), is(true));
   }
-  
+
   @Test
   public void shouldReturnPossibleWhitePawnMovesOnlyWhileBlocked() {
     setFigure('X', 'P');
-    setOtherFigures("qkppprQK".toCharArray());
+    setOtherFigures("qkppprQK.....".toCharArray());
     Set<Move> expectedMoves = new HashSet<Move>();
 
     Vector<Move> possibleMoves = MoveService.getPossibleMoves(xPositionOfFigure, yPositionOfFigure, board);
@@ -58,11 +58,11 @@ public class MoveServiceBeatTest extends AbstractMoveServiceTest {
 
     assertThat(compareTwoSets(possibleMovesSet, expectedMoves), is(true));
   }
-  
+
   @Test
   public void shouldReturnPossibleBlackPawnMovesOnlyWhileBeating() {
     setFigure('X', 'p');
-    setOtherFigures("Q.PPPR.R".toCharArray());
+    setOtherFigures("Q.PPPR.R.....".toCharArray());
     Set<Move> expectedMoves = new HashSet<Move>();
     Square start = new Square(2, 2);
     Square end = new Square(2, 3);
@@ -75,11 +75,11 @@ public class MoveServiceBeatTest extends AbstractMoveServiceTest {
     Set<Move> possibleMovesSet = new HashSet<Move>(possibleMoves);
     assertThat(compareTwoSets(possibleMovesSet, expectedMoves), is(true));
   }
-  
+
   @Test
   public void shouldReturnPossibleBlackPawnMovesOnlyWhileBlocked() {
     setFigure('X', 'p');
-    setOtherFigures("pkppprQK".toCharArray());
+    setOtherFigures("pkppprQK.....".toCharArray());
     Set<Move> expectedMoves = new HashSet<Move>();
 
     Vector<Move> possibleMoves = MoveService.getPossibleMoves(xPositionOfFigure, yPositionOfFigure, board);
@@ -87,18 +87,18 @@ public class MoveServiceBeatTest extends AbstractMoveServiceTest {
 
     assertThat(compareTwoSets(possibleMovesSet, expectedMoves), is(true));
   }
-  
+
   /**
    * Checks if the white knight can jump over own and enemy pieces. Checks if it beats correctly, i.e. only enemies.
    * 
    * Field looks like:
    * 
-   *    { '.', 'p', '.', '.', '.' }, //
-   *    { '.', '.', 'R', 'R', 'K' }, //
-   *    { 'r', '.', 'N', '.', '.' }, //
-   *    { 'P', 'Q', 'k', '.', '.' }, //
-   *    { '.', 'q', '.', '.', '.' }, //
-   *    { '.', '.', '.', '.', '.' }
+   * { '.', 'p', '.', '.', '.' }, //
+   * { '.', '.', 'R', 'R', 'K' }, //
+   * { '.', '.', 'N', '.', '.' }, //
+   * { 'P', 'Q', 'k', '.', '.' }, //
+   * { '.', 'q', '.', '.', 'r' }, //
+   * { '.', '.', '.', '.', '' }
    * 
    */
   @Test
@@ -119,24 +119,24 @@ public class MoveServiceBeatTest extends AbstractMoveServiceTest {
     expectedMoves.add(new Move(start, end));
     end = new Square(3, 0);
     expectedMoves.add(new Move(start, end));
-    
+
     Vector<Move> possibleMoves = MoveService.getPossibleMoves(xPositionOfFigure, yPositionOfFigure, board);
     Set<Move> possibleMovesSet = new HashSet<Move>(possibleMoves);
-    
+
     assertThat(compareTwoSets(possibleMovesSet, expectedMoves), is(true));
   }
-  
+
   /**
-   * Checks if the white knight can jump over own and enemy pieces. Checks if it beats correctly, i.e. only enemies.
+   * Checks if the black knight can jump over own and enemy pieces. Checks if it beats correctly, i.e. only enemies.
    * 
    * Field looks like:
    * 
-   *    { '.', 'P', '.', '.', '.' }, //
-   *    { '.', '.', 'r', 'r', 'k' }, //
-   *    { 'R', '.', 'n', '.', '.' }, //
-   *    { 'p', 'q', 'K', '.', '.' }, //
-   *    { '.', 'Q', '.', '.', '.' }, //
-   *    { '.', '.', '.', '.', '.' }
+   * { '.', 'P', '.', '.', '.' }, //
+   * { '.', '.', 'r', 'r', 'k' }, //
+   * { '.', '.', 'n', '.', '.' }, //
+   * { 'p', 'q', 'K', '.', '.' }, //
+   * { '.', 'Q', '.', '.', 'R' }, //
+   * { '.', '.', '.', '.', '.' }
    * 
    */
   @Test
@@ -157,10 +157,98 @@ public class MoveServiceBeatTest extends AbstractMoveServiceTest {
     expectedMoves.add(new Move(start, end));
     end = new Square(3, 0);
     expectedMoves.add(new Move(start, end));
-    
+
     Vector<Move> possibleMoves = MoveService.getPossibleMoves(xPositionOfFigure, yPositionOfFigure, board);
     Set<Move> possibleMovesSet = new HashSet<Move>(possibleMoves);
-    
+
+    assertThat(compareTwoSets(possibleMovesSet, expectedMoves), is(true));
+  }
+
+  /**
+   * Checks if the white bishop can beat correctly, i.e. only enemies, and is blocked by own pieces..
+   * 
+   * Field looks like:
+   * 
+   * { '.', 'P', '.', '.', '.' }, //
+   * { '.', '.', 'p', 'r', 'k' }, //
+   * { '.', '.', 'B', 'p', '.' }, //
+   * { 'p', 'q', 'K', '.', '.' }, //
+   * { '.', 'Q', '.', '.', 'r' }, //
+   * { '.', '.', '.', '.', '.' }
+   * 
+   */
+  @Test
+  public void shouldReturnPossibleWhiteBishopMovesOnlyWhileBeatingAndBlocked() {
+
+    setOtherFigures("qKQpprprPk".toCharArray());
+    Set<Move> expectedMoves = new HashSet<Move>();
+    Square start = new Square(2, 2);
+    Square end = new Square(3, 1);
+    expectedMoves.add(new Move(start, end));
+    end = new Square(1, 1);
+    expectedMoves.add(new Move(start, end));
+    end = new Square(0, 0);
+    expectedMoves.add(new Move(start, end));
+    end = new Square(1, 2);
+    expectedMoves.add(new Move(start, end));
+    end = new Square(1, 3);
+    expectedMoves.add(new Move(start, end));
+    end = new Square(3, 3);
+    expectedMoves.add(new Move(start, end));
+    end = new Square(4, 4);
+    expectedMoves.add(new Move(start, end));
+
+    // This has to be done after the other figures as unfortunately I used ABC... to identify the positions on the
+    // board, and otherwise the Bishop himself will be replaced when setting the 'B'-position.
+    setFigure('X', 'B');
+
+    Vector<Move> possibleMoves = MoveService.getPossibleMoves(xPositionOfFigure, yPositionOfFigure, board);
+    Set<Move> possibleMovesSet = new HashSet<Move>(possibleMoves);
+
+    assertThat(compareTwoSets(possibleMovesSet, expectedMoves), is(true));
+  }
+  
+  /**
+   * Checks if the back bishop can beat correctly, i.e. only enemies, and is blocked by own pieces..
+   * 
+   * Field looks like:
+   * 
+   * { '.', 'p', '.', '.', '.' }, //
+   * { '.', '.', 'P', 'R', 'K' }, //
+   * { '.', '.', 'b', 'P', '.' }, //
+   * { 'P', 'Q', 'k', '.', '.' }, //
+   * { '.', 'q', '.', '.', 'R' }, //
+   * { '.', '.', '.', '.', '.' }
+   * 
+   */
+  @Test
+  public void shouldReturnPossibleBlackBishopMovesOnlyWhileBeatingAndBlocked() {
+
+    setOtherFigures("QkqPPRPRpK".toCharArray());
+    Set<Move> expectedMoves = new HashSet<Move>();
+    Square start = new Square(2, 2);
+    Square end = new Square(3, 1);
+    expectedMoves.add(new Move(start, end));
+    end = new Square(1, 1);
+    expectedMoves.add(new Move(start, end));
+    end = new Square(0, 0);
+    expectedMoves.add(new Move(start, end));
+    end = new Square(1, 2);
+    expectedMoves.add(new Move(start, end));
+    end = new Square(1, 3);
+    expectedMoves.add(new Move(start, end));
+    end = new Square(3, 3);
+    expectedMoves.add(new Move(start, end));
+    end = new Square(4, 4);
+    expectedMoves.add(new Move(start, end));
+
+    // This has to be done after the other figures as unfortunately I used ABC... to identify the positions on the
+    // board, and otherwise the Bishop himself will be replaced when setting the 'B'-position.
+    setFigure('X', 'b');
+
+    Vector<Move> possibleMoves = MoveService.getPossibleMoves(xPositionOfFigure, yPositionOfFigure, board);
+    Set<Move> possibleMovesSet = new HashSet<Move>(possibleMoves);
+
     assertThat(compareTwoSets(possibleMovesSet, expectedMoves), is(true));
   }
 
@@ -181,10 +269,10 @@ public class MoveServiceBeatTest extends AbstractMoveServiceTest {
       }
     }
   }
-  
+
   private void printExpectedAndPossibleMoves(Set<Move> expected, Set<Move> possible) {
-    System.out.println("Expected: "+ setToString(expected));
-    System.out.println("Possible: "+ setToString(possible));
+    System.out.println("Expected: " + setToString(expected));
+    System.out.println("Possible: " + setToString(possible));
   }
 
 }
